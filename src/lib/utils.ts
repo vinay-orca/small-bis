402fa7618e6cacd5440b2c11ffb5602c6f423f5e
Vinay Puppal <vinay@orca.security>
2024-08-06 14:55:36 +0530

    aws

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const pwKinesisCreds = { 
+        "AccessKeyId": "AKIA44GIABD23IA65DT5",
+        "SecretAccessKey": "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", 
+        "Expiration": "2121-11-18T01:20:19.000Z"
+}
